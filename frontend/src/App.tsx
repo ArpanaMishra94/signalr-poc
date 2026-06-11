a2192712fae6790ca015b0f71ba490a444e6dce1
Arpana Mishra <arpanamishra394@gmail.com>
2026-06-11 10:50:47 +0530

    feat: implemented lazy loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
-import IncompleteRecordsPage
-from "./features/incompleteRecords/components/IncompleteRecordsPage";
+import { lazy, Suspense } from "react";
+// import IncompleteRecordsPage from "./features/incompleteRecords/components/IncompleteRecordsPage";
+
+const IncompleteRecordsPage = lazy(
+  () =>
+    import(
+      "./features/incompleteRecords/components/IncompleteRecordsPage"
+    )
+);
 
 function App() {
-  return <IncompleteRecordsPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <IncompleteRecordsPage />
+    </Suspense>
+  );
 }
 
 export default App;
